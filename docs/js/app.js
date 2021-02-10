37fe887ea01a8bc079c51df47022c167775ff9e7
Raybox-live-autopool <78815790+Raybox-live-autopool@users.noreply.github.com>
2021-02-10 14:46:43 +0100

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -13,10 +13,12 @@ App = {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
+      web3.eth.defaultAccount = web3.eth.accounts[0]
     } else {
       // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
+      web3.eth.defaultAccount = web3.eth.accounts[0]
     }
     return App.initContracts();
   },
